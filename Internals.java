1c5f526d15f40e5bdc68899402b69a10b47913d1
krithikahkotian <krithikah.cs23@bmsce.ac.in>
2024-11-11 15:58:02 +0530

    Update Internals.java

--- Internals.java
+++ Internals.java
@@ -1,38 +1,22 @@
-package SEE;
+package CIE;
 
-import CIE.Internals;
 import java.util.Scanner;
 
-public class Externals extends Internals {
-    protected int smarks[] = new int[5];    // SEE marks
-    protected int finalMarks[] = new int[5]; // Final marks (CIE + SEE)
-
-        public Externals() {
-        super();  
-    }
-
-    
-    public void inputSEEmarks() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 5 Subject SEE marks: ");
+public class Internals extends Student {
+    protected int marks[] = new int[5];  
+    public void inputCIEmarks() {
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("Enter 5 Subject CIE marks: ");
         for (int i = 0; i < 5; i++) {
-            smarks[i] = sc.nextInt();  // Store each subject's SEE marks
+            marks[i] = sc.nextInt(); 
         }
-         
+       
     }
 
-        public void calculateFinalMarks() {
+        public void displayCIEmarks() {
+        System.out.println("CIE Marks: ");
         for (int i = 0; i < 5; i++) {
-            finalMarks[i] = smarks[i] + marks[i];  // Final marks = CIE marks + SEE marks
-        }
-    }
-
-       public void displayFinalMarks() {
-        displayStudentDetails(); 
-        displayCIEmarks();        
-        System.out.println("Final Marks: ");
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Subject " + (i + 1) + ": " + finalMarks[i]);
+            System.out.println("Subject " + (i + 1) + ": " + marks[i]);
         }
     }
 }
